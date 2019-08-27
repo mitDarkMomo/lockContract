9cf852731e1de559e7d87dfc3d8b3bd68570aef1
mitDarkMomo <18507487716@163.com>
2019-08-27 11:51:35 +0800

    add rate check

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -16,6 +16,7 @@ contract Lock {
     }
     
     mapping (address => lockInfo) public pays;
+    mapping (uint256 => mapping(uint256 => uint256)) public rates;    //uint256 => uint256 => uint256;
     
     address owner;
     
@@ -26,6 +27,22 @@ contract Lock {
     
     constructor() public payable {
         owner = msg.sender;
+
+        rates[3][0] = 250;  // X < 100W
+        rates[3][1] = 275;  // 100W <= X < 1000W
+        rates[3][2] = 325;  // X >= 1000W
+
+        rates[6][0] = 275;  // X < 100W
+        rates[6][1] = 300;  // 100W <= X < 1000W
+        rates[6][2] = 350;  // X >= 1000W
+
+        rates[12][0] = 325; // X < 100W
+        rates[12][1] = 350; // 100W <= X < 1000W
+        rates[12][2] = 400; // X >= 1000W
+    }
+
+    function () external payable {
+
     }
     
     /**
@@ -33,10 +50,19 @@ contract Lock {
      */
     function applyQuota(uint256 months, uint256 rate) external payable {
         require(msg.value > 0, "only accept money > 0");
-        require(months == 3 || months == 6 || months == 12, "lock period should be 3,6,12");
-        require(rate < 100, "rate should be less than 100");
+        require(rates[months][0] > 0, "lock period should be in range");
+        require(pays[msg.sender]._unlockTime == 0, "one address should lock only once");
+        
+        require(rate < 10000, "rate should be less than 10000");
+        if(msg.value < 1000000 ether) {
+            require(rate == rates[months][0], "Inconsistent interest rates");
+        }else if(1000000 ether <= msg.value && msg.value < 10000000 ether) {
+            require(rate == rates[months][1], "Inconsistent interest rates");
+        }else {
+            require(rate == rates[months][2], "Inconsistent interest rates");
+        }
 
-        uint256 amountPerMonth = msg.value.mul(rate).div(100).div(12);
+        uint256 amountPerMonth = msg.value.mul(rate).div(10000).div(12);
         uint256 unlockTime = now.add(months.mul(30 days));
         uint256 nextPayTime = now.add(30 days);
         
@@ -51,31 +77,40 @@ contract Lock {
     /**
      * @dev caculate the amount to withdraw and transfer to specified address
      */ 
-     function withdraw(address payable toAddr) external onlyOwner {
-         require(toAddr != address(0), "toAddr should not be 0");
-         require(pays[msg.sender]._amountPerMonth > 0, "fromAddr should have applied for quota or be in lock time");
-         
-         calAmount(msg.sender);
-         
-         uint256 amount = pays[msg.sender]._amountToWithdraw;
-         pays[msg.sender]._amountToWithdraw = 0;
-         
-         if(pays[msg.sender]._nextPayTime > pays[msg.sender]._unlockTime) {
-             amount = amount.add(pays[msg.sender]._capital);
-             delete pays[msg.sender];
-         }
-         if(amount > 0) {
-            toAddr.transfer(amount);
-         }
-     }
+    function withdraw(address payable toAddr) external onlyOwner {
+        require(toAddr != address(0), "toAddr should not be 0");
+        require(pays[msg.sender]._amountPerMonth > 0, "fromAddr should have applied for quota or be in lock time");
+        
+        calAmount(msg.sender);
+        
+        uint256 amount = pays[msg.sender]._amountToWithdraw;
+        pays[msg.sender]._amountToWithdraw = 0;
+        
+        if(pays[msg.sender]._nextPayTime > pays[msg.sender]._unlockTime) {
+            amount = amount.add(pays[msg.sender]._capital);
+            delete pays[msg.sender];
+        }
+        if(amount > 0) {
+        toAddr.transfer(amount);
+        }
+    }
+
+    /**
+     * @dev set rate    
+     */
+    function setRate(uint256 month, uint256 flag0, uint256 flag1, uint256 flag2) external onlyOwner {
+        rates[month][0] = flag0;
+        rates[month][1] = flag1;
+        rates[month][2] = flag2;
+    }
      
      /**
       * @dev calculate amount to withdraw by specified address
       */ 
-     function calAmount(address addr) internal {
-         while(pays[addr]._nextPayTime <= now && pays[addr]._nextPayTime <= pays[addr]._unlockTime) {
-             pays[addr]._amountToWithdraw = pays[addr]._amountToWithdraw.add(pays[addr]._amountPerMonth);
-             pays[addr]._nextPayTime = pays[addr]._nextPayTime.add(30 days);
-         }
-     }
+    function calAmount(address addr) internal {
+        while(pays[addr]._nextPayTime <= now && pays[addr]._nextPayTime <= pays[addr]._unlockTime) {
+            pays[addr]._amountToWithdraw = pays[addr]._amountToWithdraw.add(pays[addr]._amountPerMonth);
+            pays[addr]._nextPayTime = pays[addr]._nextPayTime.add(30 days);
+        }
+    }
 }
--- test/lock.js
+++ test/lock.js
@@ -6,43 +6,26 @@ contract('Lock', (accounts) => {
     const months12 = 12;
     const day = 60*60*24;
   
-    it('lock period should be 3,6,12', async () => {
+    it('lock period should be 3,6,12 && rate should be less than 10000', async () => {
         const lockInstance = await Lock.deployed();
 
         
         const monthsX = 10;
-
-        const rate = 4;
-
         try{
-            await lockInstance.applyQuota(monthsX, rate, {value: web3.utils.toWei('1', 'ether')});
+            await lockInstance.applyQuota(monthsX, 250, {value: web3.utils.toWei('1', 'ether')});
         }catch(error) {
             assert.include(error.toString(), "revert", "lock period error, tx reverted");
         }
 
+        let rate = 10001;
         try{
-            await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('1', 'ether')});
-            await lockInstance.applyQuota(months6, rate, {value: web3.utils.toWei('1', 'ether')});
-            await lockInstance.applyQuota(months12, rate, {value: web3.utils.toWei('1', 'ether')});
-        }catch(error) {
-            assert(false);
-        }
-    });
-
-    it('rate should be less than 100', async () => {
-        const lockInstance = await Lock.deployed();
-
-        let rate = 101;
-
-        try{
-            await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('1', 'ether')});
+            await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('12000', 'ether')});
         }catch(error) {
             assert.include(error.toString(), "revert", "rate error, tx reverted");
         }
 
-        rate = 10;
         try{
-            await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('1', 'ether')});
+            await lockInstance.applyQuota(months3, 250, {value: web3.utils.toWei('12000', 'ether')});
         }catch(error) {
             assert(false);
         }
@@ -63,12 +46,9 @@ contract('Lock', (accounts) => {
     it('_amountPerMonth, _nextPayTime, _unlockTime should be correct', async () => {
         const lockInstance = await Lock.deployed();
 
-        let rate = 12;
-
-        await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('100', 'ether')});
         let lockInfo = await lockInstance.pays.call(accounts[0]);
-        assert.equal(web3.utils.fromWei(lockInfo[0]), 1, "amountPerMonth should be 1");
-        assert.equal(web3.utils.fromWei(lockInfo[4]), 100, "capital should be 100 ether");
+        assert.equal(web3.utils.fromWei(lockInfo[0]), 25, "amountPerMonth should be 25");
+        assert.equal(web3.utils.fromWei(lockInfo[4]), 12000, "capital should be 12000 ether");
     });
 
     it('to address should not be 0', async () => {
@@ -91,40 +71,135 @@ contract('Lock', (accounts) => {
         }
     });
 
-    it('withdraw correctly', async () => {
+    it('one address should lock only once', async () => {
         const lockInstance = await Lock.deployed();
-        
-        const rate = 12;
-        await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('10', 'ether')});
-        
-        // let balanceBefore = await web3.eth.getBalance(accounts[1]);
-        // // let balanceBefore = web3.utils.toBN(web3.utils.toWei('1500', 'ether'))
-        // await lockInstance.withdraw(accounts[1]);
-        // let balanceAfter = await web3.eth.getBalance(accounts[1]);
-        // assert.deepEqual(balanceBefore, balanceAfter, "not time for withdraw yet");
 
-        // 30 days later
-        let passby = day * 90;
-        await web3.currentProvider.send({
-            jsonrpc: "2.0", 
-            method: "evm_increaseTime", 
-            params: [passby], 
-            id: 0
-        });
-        await web3.currentProvider.send({
-            jsonrpc: "2.0", 
-            method: "evm_mine", 
-            params: [], 
-            id: 0
-        });
-        
+        let rate = 12;
+
+        // await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('10', 'ether')});
         try{
-            await lockInstance.withdraw(accounts[1]);
+            await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('10', 'ether')});
         }catch(error) {
+            assert.include(error.toString(), "revert", "lock address error, tx reverted");
+        }
+    });
 
+    it('rate should be consistent', async () => {
+        const lockInstance = await Lock.deployed();
+
+        let rate250 = 250;
+        let rate275 = 275;
+        let rate300 = 300;
+        let rate325 = 325;
+        let rate350 = 350;
+        let rate400 = 400;
+
+        //month == 3
+        try{
+            await lockInstance.applyQuota(months3, rate250+1, {from: accounts[1] ,value: web3.utils.toWei('10', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
         }
 
-        // assert.deepEqual(balanceBefore, balanceAfter, "should withdraw a month interest");
+        try{
+            await lockInstance.applyQuota(months3, rate275+1, {from: accounts[2] ,value: web3.utils.toWei('1000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        try{
+            await lockInstance.applyQuota(months3, rate325+1, {from: accounts[3] ,value: web3.utils.toWei('10000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        //month == 6
+        try{
+            await lockInstance.applyQuota(months6, rate275+1, {from: accounts[4] ,value: web3.utils.toWei('10', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        try{
+            await lockInstance.applyQuota(months6, rate300+1, {from: accounts[5] ,value: web3.utils.toWei('1000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        try{
+            await lockInstance.applyQuota(months6, rate350+1, {from: accounts[6] ,value: web3.utils.toWei('10000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        //month == 12
+        try{
+            await lockInstance.applyQuota(months12, rate325+1, {from: accounts[7] ,value: web3.utils.toWei('10', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        try{
+            await lockInstance.applyQuota(months12, rate350+1, {from: accounts[8] ,value: web3.utils.toWei('1000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        try{
+            await lockInstance.applyQuota(months12, rate400+1, {from: accounts[9] ,value: web3.utils.toWei('10000001', 'ether')});
+        }catch(error) {
+            assert.include(error.toString(), "revert", "rate consistence error, tx reverted");
+        }
+
+        await lockInstance.applyQuota(months3, rate250, {from: accounts[1] ,value: web3.utils.toWei('10', 'ether')});
+        await lockInstance.applyQuota(months3, rate275, {from: accounts[2] ,value: web3.utils.toWei('1000001', 'ether')});
+        await lockInstance.applyQuota(months3, rate325, {from: accounts[3] ,value: web3.utils.toWei('10000001', 'ether')});
+
+        await lockInstance.applyQuota(months6, rate275, {from: accounts[4] ,value: web3.utils.toWei('10', 'ether')});
+        await lockInstance.applyQuota(months6, rate300, {from: accounts[5] ,value: web3.utils.toWei('1000001', 'ether')});
+        await lockInstance.applyQuota(months6, rate350, {from: accounts[6] ,value: web3.utils.toWei('10000001', 'ether')});
+
+        await lockInstance.applyQuota(months12, rate325, {from: accounts[7] ,value: web3.utils.toWei('10', 'ether')});
+        await lockInstance.applyQuota(months12, rate350, {from: accounts[8] ,value: web3.utils.toWei('1000001', 'ether')});
+        await lockInstance.applyQuota(months12, rate400, {from: accounts[9] ,value: web3.utils.toWei('10000001', 'ether')});
     });
+
+    it('set rate', async () => {
+        const lockInstance = await Lock.deployed();
+
+        await lockInstance.setRate(3, 100, 200, 300);
+
+        let rate = await lockInstance.rates.call(3, 0);
+        assert.equal(rate, 100, "rates[3][0] should be 100");
+    });
+
+    // it('withdraw correctly', async () => {
+    //     const lockInstance = await Lock.deployed();
+        
+    //     const rate = 12;
+    //     await lockInstance.applyQuota(months3, rate, {value: web3.utils.toWei('10', 'ether')});
+        
+    //     // 30 days later
+    //     let passby = day * 90;
+    //     await web3.currentProvider.send({
+    //         jsonrpc: "2.0", 
+    //         method: "evm_increaseTime", 
+    //         params: [passby], 
+    //         id: 0
+    //     });
+    //     await web3.currentProvider.send({
+    //         jsonrpc: "2.0", 
+    //         method: "evm_mine", 
+    //         params: [], 
+    //         id: 0
+    //     });
+        
+    //     try{
+    //         await lockInstance.withdraw(accounts[1]);
+    //     }catch(error) {
+
+    //     }
+
+    // });
 
 });
